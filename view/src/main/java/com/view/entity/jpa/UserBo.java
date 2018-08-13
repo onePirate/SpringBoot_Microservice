00cce3ca9c7133ef8d2f1da7c201ada760135499
Divid <33879752+onePirate@users.noreply.github.com>
2018-08-13 11:37:07 +0800

    【前台系统】UserBo用户的实体类
    UserBo用户的实体类

---
+++ view/src/main/java/com/view/entity/jpa/UserBo.java
@@ -0,0 +1,36 @@
+package com.view.entity.jpa;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+
+/**
+ * @Author: gwx556610
+ * @Date: 16:37 2018/8/6
+ * @Desc:
+ */
+@Data
+@Entity
+@Table(name="t_user")
+public class UserBo implements Serializable {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "t_id")
+    private Long id;
+
+    @Column(name = "t_name")
+    private String name;
+
+    @Column(name = "t_age")
+    private int age;
+
+    @Column(name = "t_address")
+    private String address;
+
+    @Column(name = "t_pwd")
+    private String pwd;
+
+}
