17574ce8f8fa877b870637b43ae6f3dd946016bc
Divid <33879752+onePirate@users.noreply.github.com>
2018-08-13 11:40:31 +0800

    【前台系统】Session拦截器
    SessionInterceptor拦截器

---
+++ view/src/main/java/com/view/interceptor/SessionInterceptor.java
@@ -0,0 +1,32 @@
+package com.view.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author: gwx556610
+ * @Date: 10:25 2018/7/24
+ * @Desc:
+ */
+@Slf4j
+public class SessionInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("--------------[method]:"+request.getServletPath());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
