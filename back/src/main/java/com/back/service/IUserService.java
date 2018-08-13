08a9e1e22ba1b31ec38c1e3006634c6e32d6ba48
Divid <33879752+onePirate@users.noreply.github.com>
2018-08-13 14:10:01 +0800

    【后台管理系统】service层用户操作接口
    service层用户操作接口

---
+++ back/src/main/java/com/back/service/IUserService.java
@@ -0,0 +1,42 @@
+package com.back.service;
+
+import com.back.entity.UserBo;
+
+import java.util.List;
+
+/**
+ * @Author: gwx556610
+ * @Date: 14:34 2018/8/9
+ * @Desc: 用户的相关操作
+ */
+public interface IUserService {
+
+    /**
+     * 获取所有用户
+     * @return
+     */
+    List<UserBo> getAllUser();
+
+    /**
+     * 获取一个用户
+     * @param userBo
+     * @return
+     */
+    List<UserBo> getOneUser(UserBo userBo);
+
+    /**
+     * 保存用户信息
+     * @param userBo
+     */
+    void save(UserBo userBo);
+    /**
+     * 更新用户信息
+     * @param userBo
+     */
+    void update(UserBo userBo);
+    /**
+     * 删除用户信息
+     * @param userBo
+     */
+    void del(UserBo userBo);
+}
