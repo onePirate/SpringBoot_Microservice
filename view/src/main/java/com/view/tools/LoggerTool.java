4cb112d324a1f0d6f76223a7bde86c371c9c4d42
Divid <33879752+onePirate@users.noreply.github.com>
2018-08-13 12:47:56 +0800

    【前台系统】日志处理工具类
    日志处理工具类

---
+++ view/src/main/java/com/view/tools/LoggerTool.java
@@ -0,0 +1,46 @@
+package com.view.tools;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author: gwx556610
+ * @Date: 16:21 2018/8/8
+ * @Desc: 日志处理工具类
+ */
+public final class LoggerTool {
+
+    public static final String LOGGER_RETURN = "_logger_return";
+
+    private LoggerTool() {}
+
+    /**
+     * 获取客户端ip地址
+     * @param request
+     * @return
+     */
+    public static String getCliectIp(HttpServletRequest request)
+    {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.trim() == "" || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.trim() == "" || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.trim() == "" || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        // 多个路由时，取第一个非unknown的ip
+        final String[] arr = ip.split(",");
+        for (final String str : arr) {
+            if (!"unknown".equalsIgnoreCase(str)) {
+                ip = str;
+                break;
+            }
+        }
+        return ip;
+    }
+
+
+}
