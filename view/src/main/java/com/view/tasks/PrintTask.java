8d72eab96cdc287fce9b814567d2059bb3217b52
Divid <33879752+onePirate@users.noreply.github.com>
2018-08-13 12:46:40 +0800

    【前台系统】定时打印日志的任务
    定时打印日志的任务

---
+++ view/src/main/java/com/view/tasks/PrintTask.java
@@ -0,0 +1,20 @@
+package com.view.tasks;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: gwx556610
+ * @Date: 10:26 2018/8/9
+ * @Desc: 定时打印日志的任务
+ */
+@Slf4j
+@Component
+public class PrintTask {
+
+    @Scheduled(cron="10 * * * * *")
+    public void print(){
+        log.info(">>>>>打印当前时间："+System.currentTimeMillis());
+    }
+}
