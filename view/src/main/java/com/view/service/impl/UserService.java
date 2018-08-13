b080789a82ff4e431bef29a279f50dd3f4f3e0e2
Divid <33879752+onePirate@users.noreply.github.com>
2018-08-13 12:45:38 +0800

    【前台系统】用户操作实现类
    用户操作实现类

---
+++ view/src/main/java/com/view/service/impl/UserService.java
@@ -0,0 +1,33 @@
+package com.view.service.impl;
+
+import com.view.entity.jpa.UserBo;
+import com.view.jpa.UserJpa;
+import com.view.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements IUserService {
+
+    @Autowired
+    UserJpa userJpa;
+
+    @Override
+    public List<UserBo> getAllUser() {
+        return userJpa.findAll();
+    }
+
+    @Override
+    public UserBo saveOrUpdate(UserBo userBo) {
+        return userJpa.save(userBo);
+    }
+
+
+    @Override
+    public void delUser(Long id) {
+        userJpa.delete(id);
+    }
+
+}
