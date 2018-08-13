99410e8fa81a38a748ca729c0e365210ca874e16
Divid <33879752+onePirate@users.noreply.github.com>
2018-08-13 11:27:39 +0800

    【前台系统】添加跨域访问方式
    添加跨域访问方式

---
+++ view/src/main/java/com/view/config/CORSConfig.java
@@ -0,0 +1,29 @@
+package com.view.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Author: gwx556610
+ * @Date: 10:11 2018/8/9
+ * @Desc: 添加跨域访问方式
+ */
+/**
+ * addMapping：配置可以被跨域的路径，可以任意配置，可以具体到直接请求路径。
+ * allowedMethods：允许所有的请求方法访问该跨域资源服务器，如：POST、GET、PUT、DELETE等。
+ * allowedOrigins：允许所有的请求域名访问我们的跨域资源，可以固定单条或者多条内容，如："http://www.baidu.com"，只有百度可以访问我们的跨域资源。
+ * allowedHeaders：允许所有的请求header访问，可以自定义设置任意请求头信息，如："X-YAUTH-TOKEN"
+ */
+@Configuration
+public class CORSConfig extends WebMvcConfigurerAdapter
+{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+       /* registry
+                .addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*");*/
+    }
+}
