191a1b8546d22c9a8ce3518a79ed37254e7d8ad0
Divid <33879752+onePirate@users.noreply.github.com>
2018-08-13 11:30:01 +0800

    【前台系统】拦截器的使用
    SessionInterceptor，LoggerInterceptor拦截器的使用

---
+++ view/src/main/java/com/view/config/InterceptorConfig.java
@@ -0,0 +1,23 @@
+package com.view.config;
+
+import com.view.interceptor.LoggerInterceptor;
+import com.view.interceptor.SessionInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Author: gwx556610
+ * @Date: 10:27 2018/7/24
+ * @Desc: 拦截器的使用
+ */
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(new LoggerInterceptor()).addPathPatterns("/**");
+    }
+
+}
