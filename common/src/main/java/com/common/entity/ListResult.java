987840334fbcc47a6f45d84d810eec4b95455efb
Divid <33879752+onePirate@users.noreply.github.com>
2018-08-13 13:46:56 +0800

    【公共服务】List返回对象实体类
    List返回对象实体类

---
+++ common/src/main/java/com/common/entity/ListResult.java
@@ -0,0 +1,25 @@
+package com.common.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: gwx556610
+ * @Date: 17:19 2018/8/8
+ * @Desc: 如果是List则返回
+ */
+@Data
+public class ListResult {
+
+    //当前页
+    protected Integer curPage;
+
+    //总页数
+    protected Integer totalPage;
+
+    //总条数
+    protected Integer totalRow;
+
+    //列表
+    protected Object list;
+
+}
