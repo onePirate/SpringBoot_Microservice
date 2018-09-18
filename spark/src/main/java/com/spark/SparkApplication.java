d4ca1dd5329bb154d9de3200b41e163223ba6826
Divid <33879752+onePirate@users.noreply.github.com>
2018-09-18 16:10:39 +0800

    SparkApplication 启动类
    SparkApplication 启动类

---
+++ spark/src/main/java/com/spark/SparkApplication.java
@@ -0,0 +1,14 @@
+package com.spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkApplication.class, args);
+    }
+}
